8a37c807fddf399cd962c4ef7a3b6b26379e654b
Unumongmarmahub <unumongmarma1998@gmail.com>
2025-11-18 19:04:38 +0600

    solved 4th problem

--- src/solution.ts
+++ src/solution.ts
@@ -15,3 +15,34 @@ function getLength(parameter: string | string[] | number[]) {
     return parameter.length;
   }
 }
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails() {
+    const compiledResult = `Name: ${this.name}, Age: ${this.age}`;
+    return compiledResult;
+  }
+}
+
+
+
+type TItem = {
+  title: string;
+  rating: number;
+};
+
+function filterByRating(items: TItem[]): TItem[] {
+  const result = items.filter((item) => item.rating >= 4);
+  return result;
+}
+
+
+
+
